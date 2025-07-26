5f783d6c2f2bd82b71ee8fcd2984856e9e4ac1e0
Victor Cortes Figueroa <vmcortesf@gmail.com>
2025-07-26 12:13:20 -0500

    Add basic Pygame window with FPS limit and test surface rendering
    Initialize Pygame window, set caption, add frame rate limit, and render a red test surface for initial graphics testing and smoother animation.

--- main.py
+++ main.py
@@ -2,8 +2,15 @@ from sys import exit
 
 import pygame
 
+LIMIT_FPS = 60
+
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Alia's Adventure")
+clock = pygame.time.Clock()
+
+test_surface = pygame.Surface((100, 200))
+test_surface.fill("Red")
 
 
 while True:
@@ -12,6 +19,9 @@ while True:
             pygame.quit()
             exit()
 
+    screen.blit(test_surface, (200, 100))
+
     # Draw all our elements
     # update everything
     pygame.display.update()
+    clock.tick(LIMIT_FPS)
